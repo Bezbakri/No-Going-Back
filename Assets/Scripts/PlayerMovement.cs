350f6dfc87870877a6fa32fbd8322cac6c403dd0
Bezbakri <bezbakri@gmail.com>
2022-12-26 15:19:51 +0530

    PLEASE FUCKING WORK

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 20f;  // Movement speed of player
+    public Vector2 movement;  // Movement axis of player
+    public Rigidbody2D rigidbody;  // The rigidbody component
+
+    public Animator animator;  // The Animator controller
+    public float hf = 0.0f;
+    public float vf = 0.0f;
+    private bool goingUp = false;
+    private bool goingLeft = false;
+    private bool goingRight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+        hf = movement.x > 0.01f ? movement.x : movement.x < -0.01f ? 1 : 0;
+        vf = movement.y > 0.01f ? movement.y : movement.y < -0.01f ? 1 : 0;
+        if (movement.x < -0.01f)
+        {
+            this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            goingLeft = true;
+            goingRight = false;
+        }
+        else
+        {
+            this.gameObject.transform.localScale = new Vector3(1, 1, 1);
+            goingRight = true;
+            if (movement.x > 0.01f)
+            {
+                goingLeft = false;
+            }
+        }
+        if (movement.y > 0.01f)
+        {
+            goingUp = true;
+            goingLeft = false;
+            goingRight = false;
+        } 
+        else if (movement.y < -0.01f)
+        {
+            goingUp = false;
+            goingLeft = false;
+            goingRight = false;
+        }
+        if (hf == 0)
+        {
+            if (goingLeft)
+            {
+                this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+        animator.SetFloat("Horizontal", hf);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", vf);
+        animator.SetBool("GoingUp", goingUp);
+        animator.SetBool("GoingSide", goingLeft || goingRight);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
