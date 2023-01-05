0e741353a88b833667911d62f711e69bdc5ede71
Bezbakri <bezbakri@gmail.com>
2023-01-05 21:24:38 +0530

    Added an opening loading scene

---
+++ Assets/Scripts/ClosingCanvas.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClosingCanvas : MonoBehaviour
+{
+    [SerializeField] private Image openingImage;
+    public float loadTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        openingImage.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        openingImage.fillAmount += 1.0f / loadTime * Time.deltaTime;
+        if (openingImage.fillAmount >= 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/boss1.cs
+++ Assets/Scripts/boss1.cs
@@ -85,7 +85,7 @@ public class boss1 : MonoBehaviour
         Destroy(shadow);
         Destroy(lightningAnim); 
         Destroy(lightning);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         StartCoroutine(SpawnLightning());
     }
 }
