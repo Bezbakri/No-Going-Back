da06c20b55d69eee66bfe9625cfec806f6fd77ab
Bezbakri <bezbakri@gmail.com>
2023-01-06 18:57:25 +0530

    Added a script to make the camera return to the player

--- Assets/Scripts/CameraReturnToPlayer.cs
+++ Assets/Scripts/CameraReturnToPlayer.cs
@@ -1,18 +1,27 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraReturnToPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private GameObject boss;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Camera m_camera;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (boss == null)
+        {
+            var brain = m_camera.GetComponent<CinemachineBrain>();
+            var vcam = (brain == null) ? null : brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+            if (vcam != null)
+            {
+                vcam.m_Lens.OrthographicSize = 10.0f;
+                vcam.Follow = player.transform;
+            }
+        }
     }
 }
